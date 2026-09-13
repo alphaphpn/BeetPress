d1410aecffd7212c98f0eaaf53c7f7b0111aa06f
alphaphpn <alphaphpn@gmail.com>
2026-09-13 20:00:07 +0800

    change in-out attendance updates

--- app/theme/default/template-part/navbar-default.php
+++ app/theme/default/template-part/navbar-default.php
@@ -3,7 +3,7 @@
 		<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" title="User"><i class="fas fa-user fa-fw"></i></a>
 		<ul class="dropdown-menu">
 			<li><a class="dropdown-item" href="<?php if ( $the_homepage == 1) { } else { echo trim($domainhome).'/'; } ?>login"><i class='fas fa-unlock-alt'></i> Login</a></li>
-			<li><a class="dropdown-item" href="<?php if ( $the_homepage == 1) { } else { echo trim($domainhome).'/'; } ?>attendance-auth"><i class='fas fa-user-clock'></i> Work Attendance</a></li>
+			<li><a class="dropdown-item" href="<?php if ( $the_homepage == 1) { } else { echo trim($domainhome).'/'; } ?>in-out"><i class='fas fa-user-clock'></i> Work Attendance</a></li>
 			<li><hr class="dropdown-divider"></li>
 			<li><a class="dropdown-item" href="<?php if ( $the_homepage == 1) { } else { echo trim($domainhome).'/'; } ?>employee"><i class='fas fa-shield-alt'></i> Verify Employee</a></li>
 		</ul>
